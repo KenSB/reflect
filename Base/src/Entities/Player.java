bb34f3dac6ae711c51507be1386cd0902fc62074
KenSB <kennethsb.wong@gmail.com>
2019-10-04 00:13:29 -0400

    Adding entities

---
+++ Base/src/Entities/Player.java
@@ -0,0 +1,12 @@
+package Entities;
+
+public class Player extends Entity{
+    public Player(int xPos, int yPos) {
+        super(xPos, yPos);
+    }
+
+    @Override
+    public void collision() {}
+    @Override
+    public void move() {}
+}
---
+++ Base/src/Entities/Wall.java
@@ -0,0 +1,12 @@
+package Entities;
+
+public class Wall extends Entity {
+    public Wall(int xPos, int yPos) {
+        super(xPos, yPos);
+    }
+
+    @Override
+    public void collision() {}
+    @Override
+    public void move() {}
+}
